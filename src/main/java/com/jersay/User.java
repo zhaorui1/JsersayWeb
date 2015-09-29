988d9ca316416407ce9d918a95df302614e3504a
zhaorui1 <zhaorui8714@gmail.com>
2015-09-29 10:05:19 +0800

    change

---
+++ src/main/java/com/jersay/User.java
@@ -0,0 +1,40 @@
+package com.jersay;
+
+/**
+ * Created by zhaorui on 15/9/29.
+ */
+public class User {
+
+
+    public User() {
+    }
+
+    private String id;
+    private String name;
+    private String address;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
